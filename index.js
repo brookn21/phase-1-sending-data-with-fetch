2cb8b6db51cabc49158399214bdf926811f1fce7
Brook Nega <brooknega21@gmail.com>
2022-09-26 19:36:29 -0400

    commit tree c6b47d6439a75bb90e3557b1d97a564cee91c881

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+// .then(function(resp){
+//     resp,json()
+// })
+// .then(function(data){
+//     console.log(data)
+// }
+// )
+
+// const postInfo ={
+//     method: "POST",
+//         headers: {
+//             "content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name: userName,
+//             email: userEmail,
+//         }),
+// }
+function submitData(userName, userEmail){
+    fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName ,
+            email: userEmail,
+        }),
+    })
+    .then(function(resp){
+        return resp.json()
+    })
+    .then(function(ans){
+        
+    })
+    
+}
+
+fetch("http://localhost:3000/users")
+
+
+submitData()
